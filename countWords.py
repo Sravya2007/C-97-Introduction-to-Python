bfad66381bdabd5d6a39111e7adafc3546d80d40
Sravya <65549677+Sravya2007@users.noreply.github.com>
2021-04-19 15:01:29 +0530

    Counting Words

---
+++ countWords.py
@@ -0,0 +1,14 @@
+introString = input("Enter your introduction: ")
+print(introString)
+characterCount = 0
+wordCount = 1
+
+for character in introString:
+    characterCount=characterCount+1
+    if(character==' '):
+        wordCount=wordCount+1
+        
+print("Number of words in the string: ")
+print(wordCount)
+print("Number of characters in the string: ")
+print(characterCount)
